00f2fe48f18284cf6d7df48915c77aa5a950b106
sebastabera <sebas03700@hotmail.com>
2018-10-09 06:07:42 -0500

    servicio

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../servicios/heroes.service';
 
 @Component({
   selector: 'app-heroes',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _heroesServices:HeroesService) { }
 
   ngOnInit() {
   }
