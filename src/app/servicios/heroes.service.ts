cc11b9203700b78f62aa36d16f0e99801f512b34
Luis Sebastian Tabera Quintero <lstabera@personalsoft.com.co>
2019-10-18 09:11:39 -0500

    aplicacion terminada

--- src/app/servicios/heroes.service.ts
+++ src/app/servicios/heroes.service.ts
@@ -69,9 +69,11 @@ export class HeroesService {
   buscarHeroes(termino:string){
     let heroesArr:Heroe[] = [];
     termino = termino.toLowerCase();
-    for(let heroe of this.heroes){
+    for(let i = 0; i < this.heroes.length; i++){
+      let heroe = this.heroes[i];
       let nombre = heroe.nombre.toLowerCase();
       if(nombre.indexOf(termino) >= 0){
+        heroe.idx = i;
         heroesArr.push(heroe);
       }
     }
@@ -85,5 +87,6 @@ export interface Heroe {
   bio: string,
   img: string,
   aparicion: string,
-  casa: string
+  casa: string,
+  idx?: number
 }
